d41b5ddc655fdc28dfe1b3b627a4db48fc559302
Houjun Liu <hliu@shabang.cf>
2020-03-06 10:18:54 -0800

    Committing a semi-working (and overfitting!) LR Network

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from PolitiSort.network import PolitiNet
 tokenizer = Tokenizer()
 handler = DataHandler("./senators_twsc_long.csv", tokenizer)
 handler.compile(["handle", "name", "description", "status", "isDem"])
-net = PolitiNet(32,32)
+net = PolitiNet(46,46)
 net.fit(handler, epochs=1000, batch_size=128, val_split=0.00)
 
 
