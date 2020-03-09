86725454c53c3650ff87c426b5349d859241aabc
Houjun Liu <hliu@shabang.cf>
2020-03-09 11:08:29 -0700

    Buggy, yet exquisit GAN Data handling

--- PolitiSort/data/io.py
+++ PolitiSort/data/io.py
@@ -1,4 +1,6 @@
 import csv
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
 from collections import defaultdict
 
 class Tokenizer(object):
@@ -6,6 +8,10 @@ class Tokenizer(object):
         self.__vocab = {}
         self.__vocab_rev = {}
         self.__counter = 1
+
+    @property
+    def _counter(self):
+        return self.__counter
 
     def tokenize(self, string, by_char=False):
         arrayOfStrings = string.split() if not by_char else list(string)
@@ -28,6 +34,49 @@ class Tokenizer(object):
                 string = self.__vocab_rev[number]
                 arrayOfStrings.append(string)
         return arrayOfStrings
+
+
+class GANHandler(object):
+    def __init__(self, csvInput, tokenizer:Tokenizer, batch_size:int=32):
+        self.tokenizer = tokenizer
+        self.__csvInput = csvInput
+        self.__encodedData = defaultdict(list)
+        self.__isCompiled = False
+
+    @staticmethod
+    def noise(batch:int):
+        return np.random.normal(0, 1, (batch,))
+
+    def step(self, batch_size, prev=None):
+        assert int(batch_size/2) == batch_size/2, "Batch size must be divisible by 2!!"
+        if not prev:
+            prev = self.noise(batch_size/2)
+        new_indxs = np.random.randint(1, len(self.__encodedData["bigrams"]-1), batch_size/2)
+        new = self.__encodedData["bigrams"][new_indxs]
+        zeros = np.zeros(batch_size/2)
+        ones = np.ones(batch_size/2)
+        return prev, new, zeros, ones
+        
+    def compile(self, retreiveFields=["status"]):
+        with open(self.__csvInput, 'r') as df:
+            csvreader = csv.DictReader(df)
+            for row in csvreader:
+                for field in retreiveFields:
+                    if field == "isDem":
+                        self.__encodedData[field].append([0,1] if row['isDem']=='1' else [1,0])
+                    elif field == "status" or field == "description":
+                        self.__encodedData[field].append(self.tokenizer.tokenize(row[field]))
+                    else:
+                        self.__encodedData[field].append(self.tokenizer.tokenize(row[field], by_char=True))
+        for i in range(len(self.__encodedData["status"])-2):
+            check = [self.__encodedData["status"][i], self.__encodedData["status"][i+1]]
+            if check not in self.__encodedData["bigrams"]:
+                self.__encodedData["bigrams"].append(check)
+        self.__encodedData["bigrams"] = np.array(self.__encodedData["bigrams"])
+        # self.__encodedData["status"] = pad_sequences(self.__encodedData["status"], maxlen)
+        # self.__encodedData["description"] = pad_sequences(self.__encodedData["description"], maxlen)
+
+        self.__isCompiled = True
 
 
 class DataHandler(object):
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-from PolitiSort.data.io import Tokenizer, DataHandler
+from PolitiSort.data.io import Tokenizer, DataHandler, GANHandler
 from PolitiSort.network import PolitiNet
 
 
 tokenizer = Tokenizer()
-handler = DataHandler("./senators_twsc_long.csv", tokenizer)
-handler.compile(["handle", "name", "description", "status", "isDem"])
+handler = GANHandler("./senators_twsc_long.csv", tokenizer)
+handler.compile()
+handler.step(16)
+breakpoint()
 net = PolitiNet(46,46)
 net.fit(handler, epochs=1000, batch_size=128, val_split=0.00)
 
