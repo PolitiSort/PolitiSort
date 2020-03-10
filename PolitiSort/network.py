42dae9403c0b0205b75506356185fa0805c60b85
BuildTools <unconfigured@null.spigotmc.org>
2020-03-09 21:05:18 -0700

    Replaced the convolutional networks with DNNs

--- PolitiSort/network.py
+++ PolitiSort/network.py
@@ -62,25 +62,22 @@ class PolitiGen(object):
         returns: the model object
         """
         model = Sequential()
-        model.add(Conv1D(32, kernel_size=3, strides=2, input_shape=(1,), padding="same"))
-        #model.add(Dense(32), input_shape=(1,))
+        #model.add(Conv1D(32, kernel_size=3, strides=2, input_shape=(1,), padding="same"))
+        model.add(Dense(32), input_shape=(1,))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv1D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(ZeroPadding1D(padding=((0, 1))))
+        model.add(Dense(64))
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv1D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(Dense(128))
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv1D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(Dense(256))
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-
-        model.add(Flatten())
         model.add(Dense(1, activation='sigmoid'))
         return model
 
@@ -95,17 +92,14 @@ class PolitiGen(object):
 
         model = Sequential()
 
-        model.add(Dense(128 * 7 * 7, activation="relu", input_shape=noise_shape))
-        model.add(Reshape((7, 7, 128)))
-        model.add(UpSampling1D(interpolation="bilinear"))
-        model.add(Conv1D(128, kernel_size=3, padding="same"))
+        model.add(Dense(64, activation="relu", input_shape=noise_shape))
+        model.add(Dense(128))
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(UpSampling1D(interpolation="bilinear"))
-        model.add(Conv1D(64, kernel_size=3, padding="same"))
+        model.add(Dense(64))
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Conv1D(1, kernel_size=3, padding="same"))
+        model.add(Dense(1))
         model.add(Activation("tanh"))
         return model
 
