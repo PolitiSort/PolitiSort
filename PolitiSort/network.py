489afd3d9db4280f9ff65ec59efc3f835c502fc5
Houjun Liu <hliu@shabang.cf>
2020-05-11 11:16:28 -0700

    Network.py comments

--- PolitiSort/network.py
+++ PolitiSort/network.py
@@ -17,42 +17,6 @@ from tqdm import tqdm
 from .data.io import GANHandler
 
 
-class PolitiNet(object):
-    def __init__(self, handler:GANHandler, maxdesc=15, maxstatus=15, lr=5e-3):
-        self.__maxDesc = maxdesc
-        self.__maxStatus = maxstatus
-        self.modelGenerator = None
-        self.modelDiscriminator = None
-        self.model.compile(Adam(lr=lr), loss="binary_crossentropy", metrics=["acc"])
-        self.handler = handler
-
-    def __build(self):
-        # inp_bio = Input(shape=(self.__maxDesc,)) 
-
-        inp_status = Input(shape=(self.__maxStatus,)) 
-
-        inp_masked = Masking(mask_value=0.0)(inp_status)
-
-        # inp_exp_bio = Lambda(lambda x: K.expand_dims(x, axis=-1))(inp_bio)
-
-        inp_exp_status = Lambda(lambda x: K.expand_dims(x, axis=-1))(inp_masked)
-
-        net1 = LSTM(64, return_sequences=True, activation="tanh")(inp_exp_status)
-        net1 = LSTM(32, return_sequences=True, activation="tanh")(net1)
-        net1 = LSTM(16, activation="tanh")(net1)
-
-        net3 = Dense(128, activation="tanh")(net1)
-        net3 = Dense(32, activation="tanh")(net1)
-        net3 = Dense(32, activation="tanh")(net3)
-        net3 = Dense(2, activation="softmax")(net3)
-
-        model = Model(inputs=[inp_status], outputs=[net3]) 
-        return model
-
-    def fit(self, handler, epochs=10, batch_size=32, val_split=0.05):
-        input_data = handler()
-        self.model.fit(x=[pad_sequences(input_data["status"], maxlen=self.__maxStatus)], y=[input_data["isDem"]], epochs=epochs, batch_size=batch_size, validation_split=val_split, shuffle=True)
-
 class PolitiGen(object):
     def __init__(self, handler:GANHandler):
         self.handler = handler
@@ -64,15 +28,22 @@ class PolitiGen(object):
 
         returns: the model object
         """
+        # We seed a Sequential network
         model = Sequential()
-        model.add(Lambda(lambda x: K.expand_dims(x, axis=-1)))
+        # Expand the sequential input to temporal convolution slices by expanding dims
+        model.add(Lambda(lambda x: K.expand_dims(x, axis=-1))) # [1, 2, 3] => [[1], [2], [3]]
+        # <Conv1D Net>
         model.add(Conv1D(64, 5, activation=LeakyReLU()))
         model.add(Conv1D(128, 3, activation=LeakyReLU()))
+        # </Conv1D Net>
+        # Flatten ConvNet into Sequential Object
         model.add(Flatten())
+        # <DNN>
         model.add(Dense(64, activation=LeakyReLU()))
         model.add(Dense(16, activation=LeakyReLU()))
         model.add(Dense(8, activation=LeakyReLU()))
         model.add(Dense(1, activation='sigmoid'))
+        # </DNN>
         return model
 
     def __build_generator(self):
@@ -82,20 +53,30 @@ class PolitiGen(object):
 
         returns: the model object
         """
-        noise_shape = (100,)
-	
+
+        # We seed a Sequential network
         model = Sequential()
-        model.add(Lambda(lambda x: K.expand_dims(x, axis=-1)))
+        # Expand the sequential input to temporal convolution slices by expanding dims
+        model.add(Lambda(lambda x: K.expand_dims(x, axis=-1))) # [1, 2, 3] => [[1], [2], [3]]
+        # <Conv1D Net>
         model.add(Conv1D(128, 3))
         model.add(Conv1D(64, 5))
+        # </Conv1D Net>
+        # Upsample the data to force synthesis
         model.add(UpSampling1D(size=4))
+        # <Conv1D Net>
         model.add(Conv1D(64, 7))
         model.add(Conv1D(32, 9))
+        # </Conv1D Net>
+        # Upsample the data to force synthesis
         model.add(UpSampling1D())
+        # Flatten ConvNet into Sequential Object
         model.add(Flatten())
+        # <DNN>
         model.add(Dense(568))
         model.add(Dense(256))
         model.add(Dense(100))
+        # </DNN>
         return model
 
     def __compile(self):
@@ -138,8 +119,17 @@ class PolitiGen(object):
         return generator, discriminator, combined
 
     def __unison_shuffled_copies(self, a, b):
+        """
+        Unison shuffling two numpy arrays
+
+        returns: a uniformly shuffled two numy arrays
+        """
+
+        # Check that the lengths are equal, otherwise this may not be very helpful
         assert len(a) == len(b)
+        # Take a permutation nPn, which effectively returns a list of length len(a) in random order 
         p = np.random.permutation(len(a))
+        # Re-Index inputs to that random list order
         return a[p], b[p]
 
     def train(self, epochs=100, iterations=1024, batch_size=128, reporting=50):
@@ -165,6 +155,40 @@ class PolitiGen(object):
                 if i%reporting == 0:
                     print("i={}, Disc loss (R): {}, Disc loss (F): {}, Gen loss: {}, Words: {}, Disc Out (Sample Fake): {}, Disc Out (Sample Real): {}".format(i, d_loss_real[0], d_loss_fake[0], g_loss, str(generated_pairs_translated), d_res_fake[0][0], d_res_real[0][0]))
                 
+# <Here lies what was PolitiNet, a Polticial Stance Sorting thing>
+# class PolitiNet(object):
+    # def __init__(self, handler:GANHandler, maxdesc=15, maxstatus=15, lr=5e-3):
+        # self.__maxDesc = maxdesc
+        # self.__maxStatus = maxstatus
+        # self.modelGenerator = None
+        # self.modelDiscriminator = None
+        # self.model.compile(Adam(lr=lr), loss="binary_crossentropy", metrics=["acc"])
+        # self.handler = handler
 
-if __name__ == "__main__":
-    TestPolyGen = PolitiGen()
+    # def __build(self):
+        # # inp_bio = Input(shape=(self.__maxDesc,)) 
+
+        # inp_status = Input(shape=(self.__maxStatus,)) 
+
+        # inp_masked = Masking(mask_value=0.0)(inp_status)
+
+        # # inp_exp_bio = Lambda(lambda x: K.expand_dims(x, axis=-1))(inp_bio)
+
+        # inp_exp_status = Lambda(lambda x: K.expand_dims(x, axis=-1))(inp_masked)
+
+        # net1 = LSTM(64, return_sequences=True, activation="tanh")(inp_exp_status)
+        # net1 = LSTM(32, return_sequences=True, activation="tanh")(net1)
+        # net1 = LSTM(16, activation="tanh")(net1)
+
+        # net3 = Dense(128, activation="tanh")(net1)
+        # net3 = Dense(32, activation="tanh")(net1)
+        # net3 = Dense(32, activation="tanh")(net3)
+        # net3 = Dense(2, activation="softmax")(net3)
+
+        # model = Model(inputs=[inp_status], outputs=[net3]) 
+        # return model
+
+    # def fit(self, handler, epochs=10, batch_size=32, val_split=0.05):
+        # input_data = handler()
+        # self.model.fit(x=[pad_sequences(input_data["status"], maxlen=self.__maxStatus)], y=[input_data["isDem"]], epochs=epochs, batch_size=batch_size, validation_split=val_split, shuffle=True)
+# </Here lies what was PolitiNet, a Polticial Stance Sorting thing>
