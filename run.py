78a0837957f10c49e0459ed1a485f234bf1fef3c
Houjun Liu <hliu@shabang.cf>
2020-05-14 09:42:47 -0700

    Moved twitter get and set command args

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
-from PolitiSort.data.io import Tokenizer, DataHandler, GANHandler
-from PolitiSort.network import PolitiGen
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', FutureWarning)
+    from PolitiSort.data.io import Tokenizer, DataHandler, GANHandler
+    from PolitiSort.network import PolitiGen
 import pickle
 import sys
+import argparse
 
-tokenizer = Tokenizer("./static/1billion_word_vectors")
-handler = GANHandler("./senators_twsc_long.csv", tokenizer)
-handler.compile()
+parser = argparse.ArgumentParser("PolitiSort")
+parser.add_argument("command", help="[compile] corpus/[train] model/[scrape] dataset", type=int)
+parser.add_argument("-i", "--input", help="Input file path. Either the Corpus, Compiled Data, or Raw Acounts", type=int)
+parser.add_argument("-o", "--output", help="Output file path. Either the Corpus, Compiled Data, or Network HDH5", type=int)
+parser.add_argument("--key", help="A JSON String acquired from Twitter shaped {'CONSUMER_KEY': '', 'CONSUMER_SECRET': '', 'ACCESS_KEY': '', 'ACCESS_SECRET': ''}")
+args = parser.parse_args()
+
+print(args)
+
+# tokenizer = Tokenizer("./static/1billion_word_vectors")
+# handler = GANHandler("./senators_twsc_long.csv", tokenizer)
+# handler.compile()
 #with open("DH.GANHandler", "wb") as df:
 #    pickle.dump(handler, df)
 # breakpoint()
