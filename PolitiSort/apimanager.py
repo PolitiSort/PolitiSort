6860451fcc1362aa8ad2cd7162b41014a446d444
zbuster05 <zakary.saya@gmail.com>
2020-05-27 11:51:06 -0700

    added parameters and doc comments for most functions Merge conflicts :sunglasses:

--- PolitiSort/apimanager.py
+++ PolitiSort/apimanager.py
@@ -1,10 +1,27 @@
 from . import network
+from .data import hydrate
 
-def scrape():
+def scrape(input, output, key):
+    '''
+    Function scrape scrapes Twitter using the provided snowflakes into a provided file using a provided key
+
+    :param input: The snowflakes of the tweets that are to be scraped.
+    :param output: The destination file that this data is to be scraped to.
+    :param key: Key used to scrape Twitter.
+    :return:
+    '''
     pass
-def trainandsave():
+def createModel():
     pass
-def compile():
+def compile(inputFile, outputFile):
+    '''
+    The compile function takes the passed CSV and creates a handler that will then be dumped into a passed pickle path
+
+    :param inputFile: The path to the CSV file to be tokenized.
+    :param outputFile: The path of the pickle that the handler will be dumped into.
+    :return:
+    '''
+
     pass
-def generate():
+def generate(seed, input, sentence_count):
     pass
